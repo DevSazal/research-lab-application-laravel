7c1e32ac18c892d7af1d792d4baa9164b47e9871
DevSazal <sajal.efran@gmail.com>
2019-02-18 06:57:11 +0600

    fixed bug to add rule

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -25,7 +25,7 @@ class AdminController extends Controller
 	{
 	    $validator = Validator::make($request->all(), [
 	        'name' => 'required|string|max:255',
-	        'email' => 'required|string|email|mak:255|unique:users',
+	        'email' => 'required|string|email|max:255|unique:users',
 	        'password' => 'required|string|min:6|confirmed',
             'phone'=>  'required',
             'designation'=>  'required',
