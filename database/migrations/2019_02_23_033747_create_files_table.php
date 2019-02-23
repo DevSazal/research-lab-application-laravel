93ecc383434024b74cf6433f26a66de843697189
DevSazal <sajal.efran@gmail.com>
2019-02-23 09:42:24 +0600

    migrate for File model

--- database/migrations/2019_02_23_033747_create_files_table.php
+++ database/migrations/2019_02_23_033747_create_files_table.php
@@ -15,6 +15,8 @@ class CreateFilesTable extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('research_id');
+            $table->string('file')->nullable();
             $table->timestamps();
         });
     }
