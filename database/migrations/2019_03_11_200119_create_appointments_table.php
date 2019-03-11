5aeb9c5e6efec76b4a887a92a5f1017284a56280
DevSazal <sajal.efran@gmail.com>
2019-03-11 20:09:38 +0600

    migrate appointment model

---
+++ database/migrations/2019_03_11_200119_create_appointments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('time_id');
+            $table->integer('user_id');
+            $table->integer('m_user_id');
+            $table->dateTime('appointment_date');
+            $table->integer('research_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
