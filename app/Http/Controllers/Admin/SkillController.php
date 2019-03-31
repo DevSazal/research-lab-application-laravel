d7d0244400a67788172029ea1171a0eae9904c0d
DevSazal <sajal.efran@gmail.com>
2019-03-31 09:19:13 +0600

    authorize skill controller only for admin

--- app/Http/Controllers/Admin/SkillController.php
+++ app/Http/Controllers/Admin/SkillController.php
@@ -14,6 +14,7 @@ class SkillController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('role');
     }
     /**
      * Display a listing of the resource.
