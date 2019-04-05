2979990e7b9465a07504e9a6f6ba105336fa435d
DevSazal <sajal.efran@gmail.com>
2019-04-06 03:26:09 +0600

    implement reset email route

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -8,12 +8,19 @@
 					<img src="{{ asset('auth/images/img-01.png') }}" alt="IMG">
 				</div>
 
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" method="POST" action="{{ route('password.email') }}">
+                        @csrf
 					<span class="login100-form-title">
 						Reset Password
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					@if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+					<div class="wrap-input100 validate-input {{ $errors->has('email') ? 'validate-input alert-validate' : '' }}" data-validate = "{{ $errors->has('email') ? $errors->first('email') : 'Email is required: ex@abc.xyz' }}">
 						<input type="email" name="email" class="input100"  placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -23,21 +30,21 @@
 					
 					<div class="container-login100-form-btn">
 						<button type="submit" class="login100-form-btn">
-							Send Password Reset Link
+						{{ __('Send Password Reset Link') }}
 						</button>
 					</div>
 
 					<div class="text-center p-t-12">
-						<span class="txt1">
+						<span class="txt1"> 
 							
 						</span>
-						<a class="txt2" href="index.html">
+						<a class="txt2" href="{{ route('login') }}">
 							Login your account
 						</a>
 					</div>
 
 					<div class="text-center p-t-136">
-						<a class="txt2" href="register.html">
+						<a class="txt2" href="{{ route('register') }}">
 							<b>Create your Account</b>
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
