81f14515798b85e10e7d4b17cae9e460d69fbce0
DevSazal <sajal.efran@gmail.com>
2019-04-11 02:37:39 +0600

    migrate training model

---
+++ database/migrations/2019_04_11_022743_create_trainings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->boolean('type')->default(0);
+            $table->dateTime('start_date');
+            $table->string('trainer_name');
+            $table->longText('trainer_description')->nullable();
+            $table->integer('fee')->nullable();
+            $table->string('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+}
